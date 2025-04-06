cf16df7afc7264652b84a567420efa0e1d9244df
Ruben Adema <44266876+RadiatedMonkey@users.noreply.github.com>
2025-04-07 00:23:43 +0200

    visualize in physical coordinates rather than controllable canonical space

--- cart.py
+++ cart.py
@@ -5,21 +5,31 @@ import matplotlib.animation as animation
 
 from scipy.integrate import solve_ivp
 
-x0 = np.array([1.0, 0.0, -35 * (np.pi / 180), 0.0])
+# The initial condition.
+x0 = np.array([0.0, 0.0, -35 * (np.pi / 180), 0.0])
+
+# Parameters.
 m = 1
-M = 2
+M = 10
 g = 9.81
 l = 1
 
-X = 0.5
-Y = 1.5
-Z = 2
-W = 1
-t_span = (0, 10)
+# These are the eigenvalues we want the system to have (multiplied by -1).
+# e1 = 3/2
+# e2 = 3/4
+# e3 = 1/2
+# e4 = 1
+e1 = 3
+e2 = 4
+e3 = 1
+e4 = 2
+t_span = (0, 5)
 
+# The a and b constants defined in the article.
 a = m * g / M
 b = (M + m) * g / (M * l)
 
+# Linearized matrix of the system
 A = np.array([
     [0, 1, 0, 0],
     [0, 0, m*g / M, 0],
@@ -27,6 +37,7 @@ A = np.array([
     [0, 0, (M + m)*g/ M * l, 0]
 ])
 
+# The linearized input matrix.
 B = np.array([
     0,
     1 / M,
@@ -34,8 +45,7 @@ B = np.array([
     -1 / (M * l)
 ])
 
-print(f"Rank of B is {np.linalg.matrix_rank(B)}")
-
+# These are the q_n vectors used to create the similarity transformation.
 qs = np.zeros(4, dtype = object)
 qs[3] = B
 qs[2] = A @ B
@@ -43,39 +53,40 @@ qs[1] = (A @ A) @ B - b * B
 qs[0] = (A @ A @ A) @ B - b * (A @ B)
 
 # Convert from column major to row major.
-Tinv = np.transpose(np.array([qs[0], qs[1], qs[2], qs[3]]))
-T = np.linalg.inv(Tinv)
+T_inv = np.transpose(np.array([qs[0], qs[1], qs[2], qs[3]]))
+T = np.linalg.inv(T_inv)
 
-Acanon = T @ A @ Tinv
-Bcanon = T @ B
+# The controllable canonical forms of A and B.
+A_canon = T @ A @ T_inv
+B_canon = T @ B
 
-print(Acanon)
+print(f"The controllable canonical form of A is \n{A_canon}\nand B is {B_canon}")
 
-C3 = X + Y + Z + W
-C2 = X * (Y + Z + W) + Y * (Z + W) + Z * W
-C1 = X * (Y * Z + Y * W + Z * W) + Y * Z * W
-C0 = X * Y * Z * W
+# Computes the coefficients of the characteristic polynomial that gives the desired eigenvalues
+# (p1, p2, p3, p4) as roots. These are just the coefficients of (s + p1)(s + p2)(s + p3)(s + p4).
+c3 = e1 + e2 + e3 + e4
+c2 = e1 * (e2 + e3 + e4) + e2 * (e3 + e4) + e3 * e4
+c1 = e1 * (e2 * e3 + e2 * e4 + e3 * e4) + e2 * e3 * e4
+c0 = e1 * e2 * e3 * e4
 
-# s^4 + 10s^3 + 35s^2 + 50s + 24
+# Determines the feedback matrix, using the previously computed coefficients.
 fs = np.zeros(4, dtype = float)
-# fs[3] = -Acanon[3, 3] - 10
-# fs[2] = -Acanon[3, 2] - 35
-# fs[1] = -Acanon[3, 1] - 50
-# fs[0] = -Acanon[3, 0] - 24
-fs[3] = -Acanon[3, 3] - C3
-fs[2] = -Acanon[3, 2] - C2
-fs[1] = -Acanon[3, 1] - C1
-fs[0] = -Acanon[3, 0] - C0
+fs[3] = -A_canon[3, 3] - c3
+fs[2] = -A_canon[3, 2] - c2
+fs[1] = -A_canon[3, 1] - c1
+fs[0] = -A_canon[3, 0] - c0
 
-Fcanon = np.array([fs[0], fs[1], fs[2], fs[3]])
-F = Fcanon @ T
+# The initial feedback matrix is in canonical form, we need to
+# transform it back to regular space.
+F_canon = np.array([fs[0], fs[1], fs[2], fs[3]])
 
-Acontrolled = Acanon + np.outer(Bcanon, Fcanon)
-print(Acontrolled)
-print(np.linalg.eigvals(Acontrolled))
+# Determines the full stabilized system by adding the feedback matrix as input.
+A_controlled = T_inv @ (A_canon + np.outer(B_canon, F_canon)) @ T
+print(f"The stabilised system matrix is \n{A_controlled}")
+print(f"with eigenvalues {np.linalg.eigvals(A_controlled)}")
 
 def dynamics(t, x):
-    return Acontrolled @ x
+    return A_controlled @ x
 
 t_eval = np.linspace(*t_span, 500)
 sol = solve_ivp(dynamics, t_span, x0, t_eval = t_eval)
@@ -84,8 +95,7 @@ plt.figure(figsize = (10, 6))
 plt.plot(sol.t, sol.y.T)
 plt.xlabel("Time (s)")
 plt.ylabel("States")
-plt.legend(["x (cart pos)", "$\\dot{x}$ (cart velocity)", "$\\theta$ (angle)", "$\\dot{\\theta}}$ (angular velocity)"])
-# plt.show()
+plt.legend(["x (cart position)", "$\\dot{x}$ (cart velocity)", "$\\theta$ (angle)", "$\\dot{\\theta}}$ (angular velocity)"])
 
 cart_width = 0.5
 cart_height = 0.3
@@ -130,5 +140,7 @@ anim = animation.FuncAnimation(
     fig, update_state, frames = len(t_eval), #init_func = init_state,
     interval = (t_span[1] - t_span[0]), blit = True
 )
+
+# anim.save(filename = "C://Users/Ruben/Documents/programming/py/chaos_theory/pendulum.gif", writer = animation.PillowWriter(fps = 30))
 
 plt.show()
